a13d41d51a39493fcf39ba22a852d5b9beae4eee
RuanTorress <ruanfabio59@gmail.com>
2024-11-14 18:41:38 -0300

    Modificações no arquivo vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/LandingPageBarbearia/',
+  base: '/BarberLandig/',
   plugins: [react()],
   build: {
     publicDir: 'public',
-    base: "/BarberLandig",
+    base: '/BarberLandig/',
   },
-})
+});
